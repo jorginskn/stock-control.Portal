21d234f86553c898f109a8e10fc88fef893bef1a
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-05-23 12:21:01 -0300

    feat: SC-07 => Criação de   toastr e logica para usuário fazer login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -25,7 +27,7 @@ import { ToastModule } from 'primeng/toast';
     ButtonModule,
     ToastModule,
   ],
-  providers: [],
+  providers: [CookieService, MessageService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,11 @@
+import { AuthService } from './../../services/user/Auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
+import { AuthRequest } from 'src/app/models/interfaces/user/Auth/AuthRequest';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +15,12 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class HomeComponent implements OnInit {
   loginCard = true;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private cookieService: CookieService,
+    private messageService: MessageService
+  ) {}
 
   loginForm = this.formBuilder.group({
     email: ['', Validators.required],
@@ -17,19 +28,69 @@ export class HomeComponent implements OnInit {
   });
 
   signupForm = this.formBuilder.group({
-      name:['', Validators.required],
-      email:['', Validators.required],
-      password:['', Validators.required],
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
   });
 
   ngOnInit() {}
 
   onSubmitLoginForm(): void {
-    console.log(this.loginForm.valid)
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.authService.authUser(this.loginForm.value as AuthRequest).subscribe({
+        next: (response) => {
+          if (response) {
+            this.cookieService.set('USER_INFO', response?.token);
+            this.loginForm.reset();
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: `Bem vindo de volta ${response?.name}!`,
+              life: 2000,
+            });
+          }
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: `Erro ao fazer login! Email ou senha estão incorretos.`,
+            life: 2000,
+          });
+          console.log(err);
+        },
+      });
+    }
   }
 
   onSubmitsignUp(): void {
-
-    console.log('Cadastrado',this.signupForm)
+    debugger;
+    if (this.signupForm.value && this.signupForm.valid) {
+      this.authService
+        .signupUser(this.signupForm.value as SignupUserRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.signupForm.reset();
+              this.loginCard = true;
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Usuario criado com sucesso',
+                life: 2000,
+              });
+            }
+          },
+          error: (err) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao criar usuário',
+              life: 2000,
+            });
+            console.log(err);
+          },
+        });
+    }
   }
 }
