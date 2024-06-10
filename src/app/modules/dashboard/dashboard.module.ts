3cc93183e1a2855bb4964df804e4857838c2318e
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-06-10 10:56:24 -0300

    feat:SC-09 => Implementação da bibliocate chart.js no projeto

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -11,6 +11,7 @@ import { CardModule } from 'primeng/card';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
+import { ChartModule } from 'primeng/chart';
 
 @NgModule({
   declarations: [DashboardHomeComponent],
@@ -25,6 +26,7 @@ import { CookieService } from 'ngx-cookie-service';
     ToolbarModule,
     CardModule,
     ToastModule,
+    ChartModule
   ],
   providers: [MessageService, CookieService],
 })
