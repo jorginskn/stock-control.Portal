65adc9e768dc3201655eb9f8af5b960f6a3957e2
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-06-10 22:27:38 -0300

    feat: SC-11 => Implementada funcionalidade de logout

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-toolbar-navigation',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class ToolbarNavigationComponent {
 
+
+  constructor(private cookieService: CookieService, private router: Router) {
+
+  }
+
+
+  handleLogouUser():void{
+      this.cookieService.delete('USER_INFO');
+      void this.router.navigate(['/home']);
+  }
 }
